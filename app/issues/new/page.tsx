ad11859b515ea297cf6999c4abe2d644d294eb29
mcgowent <[mcgowen.dev@gmail.com]>
2025-04-08 12:14:31 -0600

    Show the issues

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -23,23 +23,26 @@ const NewIssuePage = () => {
     const [error, setError] = useState('');
     const [isSubmitting, setIsSubmitting] = useState(false);
 
+    const OnSubmit = handleSubmit(async (data) => 
+      {
+        try {
+          setIsSubmitting(true);
+          await axios.post("/api/issues", data);
+          router.push("/issues")
+        } catch (error) {
+          setIsSubmitting(false);
+          setError("An unexpected error occurred. Please try again later.");
+        }
+      })
+
+
   return (
     <div className="max-w-xl">
       {error && <Callout.Root color="red" className="mb-5">
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>}
       <form className="space-y-3" 
-      onSubmit={handleSubmit(async (data) => 
-          {
-            try {
-              setIsSubmitting(true);
-              await axios.post("/api/issues", data);
-              router.push("/issues")
-            } catch (error) {
-              setIsSubmitting(false);
-              setError("An unexpected error occurred. Please try again later.");
-            }
-          })}>
+      onSubmit={OnSubmit}>
 
         <TextField.Root placeholder="Title" {...register('title')}/>
         {<ErrorMessage>{errors.title?.message}</ErrorMessage>}
