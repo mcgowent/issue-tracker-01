9101eddd6675279597d9917bf5bbdb827a5f3119
VENGEANCE\mcgow <mcgowen.wow@gmail.com>
2025-05-01 12:25:12 -0500

    Fixed some loading issues

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import IssueForm from '../../_components/IssueForm'
-import prisma from '@/prisma/client'
-import { notFound } from 'next/navigation'
+import React from "react";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
 
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: true,
+  loading: () => <IssueFormSkeleton />,
+});
 interface Props {
-    params: {
-        id: string
-    }
+  params: {
+    id: string;
+  };
 }
 
 const EditIssuePage = async ({ params }: Props) => {
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(params.id) }
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
-    })
+  if (!issue) notFound();
+  return <IssueForm issue={issue} />;
+};
 
-    if (!issue) notFound()
-  return (
-    <IssueForm issue={issue} />
-  )
-}
-
-export default EditIssuePage
+export default EditIssuePage;
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -9,15 +9,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Issue } from "@prisma/client";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
+import SimpleMDE from "react-simplemde-editor";
 
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
+// const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+//   ssr: false,
+// });
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
